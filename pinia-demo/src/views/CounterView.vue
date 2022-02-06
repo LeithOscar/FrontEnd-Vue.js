d6ffcead6d83177953cba38d0e5efb1fc915a7e5
Leith <leith.jarayseh@wolterskluwer.com>
2022-02-06 20:46:24 +0100

    piniademo

---
+++ pinia-demo/src/views/CounterView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+        Counter View: {{counterStore.counter}}
+    </h1>
+    <button  @click="counterStore.incrementBy(1)">+1</button> 
+    <button @click="counterStore.incrementBy(2)">+2</button> 
+    <button @click="counterStore.incrementBy(5)">+5</button> 
+
+</template>   
+
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
---
+++ pinia-demo/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+   <h1>
+       Home View: {{counterStore.counter}}
+   </h1>
+   <h3>
+       Home nm of changes: {{counterStore.numberOfChanges}}
+   </h3>
+
+</template>   
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
